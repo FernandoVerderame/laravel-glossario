a499891bce3b378496a7c33b5f5ae326ed06c760
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-27 13:17:34 +0100

    added index comments

--- resources/views/admin/words/index.blade.php
+++ resources/views/admin/words/index.blade.php
@@ -4,11 +4,15 @@
 
 @section('content')
 
+{{-- Header --}}
 <header class="d-flex align-items-center justify-content-between pb-4 mb-4 mt-3 border-bottom">
     <h1>Words</h1>
 </header>
 
+{{-- Words table --}}
 <table class="table table-hover table-secondary table-striped border mb-4">
+
+    {{-- Words table head --}}
     <thead class="table-dark">
         <tr class="align-middle">
             <th scope="col">#</th>
@@ -20,13 +24,18 @@
             <th scope="col">Updated</th>
             <th>
                 <div class="d-flex justify-content-end gap-2">
+
+                    {{-- Trash button --}}
                     <a href="" class="btn btn-sm btn-secondary"><i class="fa-solid fa-trash me-2"></i>Show trash</a>
 
+                    {{-- Create new word button --}}
                     <a href="{{ route('admin.words.create')}}" class="btn btn-sm btn-success"><i class="fa-solid fa-plus me-2"></i>New Word</a>
                 </div>
             </th>
         </tr>
     </thead>
+
+    {{-- Words table body --}}
     <tbody>
 
         @forelse($words as $word)
@@ -40,11 +49,16 @@
             <td>{{ $word->getFormattedDate('updated_at') }}</td>
             <td>
                 <div class="d-flex justify-content-end gap-2">
+
+                    {{-- Word show button --}}
                     <a href="{{ route('admin.words.show', $word->id) }}" class="btn btn-sm btn-primary">
                         <i class="fas fa-eye"></i>
                     </a>
+
+                    {{-- Word edit button --}}
                     <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-sm btn-warning"><i class="fa-solid fa-pencil"></i></a>
 
+                    {{-- Word delete button --}}
                     <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
                         @csrf
                         @method('DELETE')
@@ -57,7 +71,7 @@
         @empty 
             <tr>
                 <td colspan="8">
-                    <h3 class="text-center">There aren't any projects.</h3>
+                    <h3 class="text-center">There aren't any words.</h3>
                 </td>
             </tr>
         @endforelse
@@ -75,6 +89,7 @@
 
 @section('scripts')
 
+    {{-- Delete confirmation --}}
     @vite('resources/js/delete_confirmation.js')
 
 @endsection
