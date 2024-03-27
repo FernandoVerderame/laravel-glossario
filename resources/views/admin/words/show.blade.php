6654f8d424185c97b373f469cb6c0fc65786cbc7
lelz99 <emanuelesimonetti99@gmail.com>
2024-03-27 12:56:07 +0100

    added admin words show

--- resources/views/admin/words/show.blade.php
+++ resources/views/admin/words/show.blade.php
@@ -1,2 +1,37 @@
+@extends('layouts.app')
 
+@section('title', $word->term)
 
+@section('content')
+<div class="card mt-5">
+  <div class="card-header bg-white">
+    <h4 class="card-title">{{$word->term}}</h4>
+    <h6 class="card-subtitle mb-2 text-body-secondary">{{$word->technology}}</h6>
+  </div>
+  <div class="card-body">
+    <p class="card-text">{{$word->definition}}</p>
+    @if(Auth::user())
+    <div>
+      <div><strong>Data Creazione:</strong> {{$word->getFormattedDate('created_at')}}</div>
+      <div><strong>Ultima Modifca:</strong> {{$word->getFormattedDate('updated_at')}}</div>
+      <div class="card-text"><strong>Pubblicato:</strong>
+        @if($word->is_published) <i class="fa-solid fa-circle-check text-success"></i>
+        @else <i class="fa-solid fa-circle-xmark text-danger"></i> 
+        @endif
+      </div>
+    </div>
+  </div>
+  <div class="card-footer bg-white d-flex justify-content-between">
+    <a href="{{route('admin.words.index')}}" class="btn btn-primary">Torna Indietro</a>
+    <div class="d-flex gap-1">
+      <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-warning"><i class="fa-solid fa-pencil me-1"></i>Modifica</a>
+      <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can me-1"></i>Elimina</button>
+      </form>
+    </div>
+  </div>
+</div>
+@endif
+@endsection
