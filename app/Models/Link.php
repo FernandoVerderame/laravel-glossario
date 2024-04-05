80e090da29c96b966a6702972a33e6d99fcde192
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-05 12:20:38 +0200

    added links formatted date function

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -14,5 +15,10 @@ class Link extends Model
     public function word()
     {
         return $this->belongsTo(Word::class);
+    }
+
+    public function getFormattedDate($column, $format = 'd-m-Y')
+    {
+        return Carbon::create($this->$column)->format($format);
     }
 }
