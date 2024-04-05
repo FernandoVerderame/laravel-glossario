e991cc623db1f1a23962c8db26606306383004f8
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-04-05 16:22:05 +0200

    fixed index

--- resources/views/admin/words/index.blade.php
+++ resources/views/admin/words/index.blade.php
@@ -4,93 +4,107 @@
 
 @section('content')
 
-{{-- Header --}}
-<header class="d-flex align-items-center justify-content-between pb-4 mb-4 mt-3 border-bottom">
-    <h1>Words</h1>
-</header>
+    {{-- Header --}}
+    <header class="d-flex align-items-center justify-content-between pb-4 mb-4 mt-3 border-bottom">
+        <h1>Words</h1>
+    </header>
 
-{{-- Words table --}}
-<table class="table table-hover table-secondary table-striped border mb-4">
+    {{-- Words table --}}
+    <table class="table table-hover table-secondary table-striped border mb-4">
 
-    {{-- Words table head --}}
-    <thead class="table-dark">
-        <tr class="align-middle">
-            <th scope="col">#</th>
-            <th scope="col">Term</th>
-            <th scope="col">Slug</th>
-            <th scope="col">Technology</th>
-            <th scope="col">Status</th>
-            <th scope="col">Link</th>
-            <th scope="col">Tags</th>
-            <th scope="col">Created</th>
-            <th scope="col">Updated</th>
-            <th>
-                <div class="d-flex justify-content-end gap-2">
+        {{-- Words table head --}}
+        <thead class="table-dark">
+            <tr class="align-middle">
+                <th scope="col">#</th>
+                <th scope="col">Term</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Technology</th>
+                <th scope="col">Status</th>
+                <th scope="col">Link</th>
+                <th scope="col">Tags</th>
+                <th scope="col">Created</th>
+                <th scope="col">Updated</th>
+                <th>
+                    <div class="d-flex justify-content-end gap-2">
 
-                    {{-- Trash button --}}
-                    <a href="{{route('admin.words.trash')}}" class="btn btn-sm btn-secondary"><i class="fa-solid fa-trash me-2"></i>Show trash</a>
+                        {{-- Trash button --}}
+                        <a href="{{ route('admin.words.trash') }}" class="btn btn-sm btn-secondary"><i
+                                class="fa-solid fa-trash me-2"></i>Show trash</a>
 
-                    {{-- Create new word button --}}
-                    <a href="{{ route('admin.words.create')}}" class="btn btn-sm btn-success"><i class="fa-solid fa-plus me-2"></i>New Word</a>
-                </div>
-            </th>
-        </tr>
-    </thead>
-
-    {{-- Words table body --}}
-    <tbody>
-
-        @forelse($words as $word)
-        <tr class="align-middle">
-            <th scope="row">{{ $word->id }}</th>
-            <td>{{ $word->term }}</td>
-            <td>{{ $word->slug }}</td>
-            <td>{{ $word->technology }}</td>
-            <td>Link</td>
-            <td>Tag 1, Tag 2, Tag 3</td>
-            <td>
-                <div class="card-text">
-                    @if($word->is_published) <i class="fa-solid fa-circle-check text-success"></i>
-                    @else <i class="fa-solid fa-circle-xmark text-danger"></i> 
-                    @endif
-                  </div>
-            </td>
-            <td>{{ $word->getFormattedDate('created_at') }}</td>
-            <td>{{ $word->getFormattedDate('updated_at') }}</td>
-            <td>
-                <div class="d-flex justify-content-end gap-2">
-
-                    {{-- Word show button --}}
-                    <a href="{{ route('admin.words.show', $word->id) }}" class="btn btn-sm btn-primary">
-                        <i class="fas fa-eye"></i>
-                    </a>
-
-                    {{-- Word edit button --}}
-                    <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-sm btn-warning"><i class="fa-solid fa-pencil"></i></a>
-
-                    {{-- Word delete button --}}
-                    <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
-                        @csrf
-                        @method('DELETE')
-                    <button type="submit" class="btn btn-sm btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-                    </form>
-                </div>
-            </td>
-        </tr>
-
-        @empty 
-            <tr>
-                <td colspan="8">
-                    <h3 class="text-center">There aren't any words.</h3>
-                </td>
+                        {{-- Create new word button --}}
+                        <a href="{{ route('admin.words.create') }}" class="btn btn-sm btn-success"><i
+                                class="fa-solid fa-plus me-2"></i>New Word</a>
+                    </div>
+                </th>
             </tr>
-        @endforelse
+        </thead>
 
-    </tbody>
-</table>
+        {{-- Words table body --}}
+        <tbody>
+
+            @forelse($words as $word)
+                <tr class="align-middle">
+                    <th scope="row">{{ $word->id }}</th>
+                    <td>{{ $word->term }}</td>
+                    <td>{{ $word->slug }}</td>
+                    <td>{{ $word->technology }}</td>
+                    <td>
+                        <div class="card-text">
+                            @if ($word->is_published)
+                                <i class="fa-solid fa-circle-check text-success"></i>
+                            @else
+                                <i class="fa-solid fa-circle-xmark text-danger"></i>
+                            @endif
+                        </div>
+                    </td>
+                    <td>Link</td>
+                    <td>
+                        @if ($word->tag)
+                            <span class="badge" style="background-color: {{ $word->tag->color }}"> {{ $word->tag->label }}
+                            </span>
+                        @else
+                            Nessuna
+                        @endif
+                    </td>
+                    <td>{{ $word->getFormattedDate('created_at') }}</td>
+                    <td>{{ $word->getFormattedDate('updated_at') }}</td>
+                    <td>
+                        <div class="d-flex justify-content-end gap-2">
+
+                            {{-- Word show button --}}
+                            <a href="{{ route('admin.words.show', $word->id) }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-eye"></i>
+                            </a>
+
+                            {{-- Word edit button --}}
+                            <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-sm btn-warning"><i
+                                    class="fa-solid fa-pencil"></i></a>
+
+                            {{-- Word delete button --}}
+                            <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form"
+                                data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger"><i
+                                        class="fa-regular fa-trash-can"></i></button>
+                            </form>
+                        </div>
+                    </td>
+                </tr>
+
+            @empty
+                <tr>
+                    <td colspan="8">
+                        <h3 class="text-center">There aren't any words.</h3>
+                    </td>
+                </tr>
+            @endforelse
+
+        </tbody>
+    </table>
 
 
-{{-- @if($words->hasPages())
+    {{-- @if ($words->hasPages())
     {{ $words->links() }}
 @endif --}}
 
