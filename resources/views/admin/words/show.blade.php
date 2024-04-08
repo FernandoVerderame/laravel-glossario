f6077e26cc8a918328687d6f5b0f6445af2bf520
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-04-08 11:52:07 +0200

    add admin and home views, refactored show view

--- resources/views/admin/words/show.blade.php
+++ resources/views/admin/words/show.blade.php
@@ -14,7 +14,6 @@
   <!-- Card body -->
   <div class="card-body">
     <p class="card-text">{{$word->definition}}</p>
-    @if(Auth::user())
     <div>
       <div><strong>Data Creazione:</strong> {{$word->getFormattedDate('created_at')}}</div>
       <div><strong>Ultima Modifca:</strong> {{$word->getFormattedDate('updated_at')}}</div>
@@ -26,7 +25,7 @@
       <div>
         <strong>Link:</strong>
         @forelse ($word->links as $link)
-            {{$link->src}}
+            <a href="{{ $link->src }}">{{ $link->src }}</a> 
         @empty
             <span>Nessun Link collegato</span>
         @endforelse
@@ -44,7 +43,11 @@
 
   <!-- Card footer -->
   <div class="card-footer bg-white d-flex justify-content-between">
-    <a href="{{route('admin.words.index')}}" class="btn btn-primary">Torna Indietro</a>
+   <div id="button-group-show">
+    @auth
+     <a href="{{ route('guest.home') }}" class="btn btn-primary"><i class="fa-solid fa-arrow-left me-1"></i>Torna alla Home</a> 
+     <a href="{{ route('admin.words.index') }}" class="btn btn-secondary"><i class="fa-solid fa-bars me-1"></i>Torna alla lista</a> 
+    </div>
     <div class="d-flex gap-1">
       <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-warning"><i class="fa-solid fa-pencil me-1"></i>Modifica</a>
       <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form" data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
@@ -53,7 +56,9 @@
         <button type="submit" class="btn btn-danger"><i class="fa-regular fa-trash-can me-1"></i>Elimina</button>
       </form>
     </div>
+    @else
+     <a href="{{ route('guest.home') }}" class="btn btn-primary"><i class="fa-solid fa-arrow-left me-1"></i>Torna alla Home</a> 
+     @endauth
   </div>
 </div>
-@endif
 @endsection
--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,9 +1,67 @@
 @extends('layouts.app')
-@section('content')
 
-@foreach ($words as $word)
-<div>
-    {{$word->term}}
+@section('title', 'Home')
+
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        @foreach ($words as $word)
+        <div class="col-12">
+            <div class="card mb-3">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <div id="left-header-card">
+                        <h5 class="card-title">{{$word->term}}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{$word->technology}}</h6>
+                    </div>
+                        <div id="right-header-card" class="d-flex justify-content-end gap-2">
+                        @auth   
+                            {{-- Word show button --}}
+                            <a href="{{ route('admin.words.show', $word->id) }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-eye me-1"></i>
+                                Vedi nel dettaglio
+                            </a>
+
+                            {{-- Word edit button --}}
+                            <a href="{{ route('admin.words.edit', $word->id) }}" class="btn btn-sm btn-warning"><i
+                                    class="fa-solid fa-pencil me-1"></i>
+                                   Modifica
+                                </a>
+
+                            {{-- Word delete button --}}
+                            <form action="{{ route('admin.words.destroy', $word->id) }}" method="POST" class="delete-form"
+                                data-bs-toggle="modal" data-bs-target="#modal" data-word="{{ $word->term }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger"><i
+                                        class="fa-regular fa-trash-can me-1"></i>
+                                    Elimina
+                                </button>
+                            </form>
+                            @else
+                            {{-- Word show button --}}
+                            <a href="{{ route('admin.words.show', $word->id) }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-eye me-1"></i>
+                                View Details
+                            </a>
+                            @endauth
+                        </div>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">{{$word->definition}}</p>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between">
+                    <div id="left-footer">
+                        <p class="card-text"><small class="text-muted">Technology: {{$word->technology}}</small></p>
+                    </div>
+                    <div id="right-footer">
+                            <strong>Data Creazione:</strong> {{$word->getFormattedDate('created_at')}}<br/>
+                            <strong>Ultima Modifca:</strong> {{$word->getFormattedDate('updated_at')}}
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
-@endforeach
 @endsection
